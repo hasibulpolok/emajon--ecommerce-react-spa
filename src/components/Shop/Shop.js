9a0be1beecc4e5497bdcc11e600dfaa6a9f59dc7
Hasibul Polok <hasibulpolok.bdn@gmail.com>
2021-09-24 03:14:47 +0600

    products added

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,9 +1,28 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Product from '../Product/Product';
+import './Shop.css'
 
 const Shop = () => {
+    const [products, setProducts] = useState([]);
+    useEffect(() => {
+        fetch('./products.JSON')
+            .then(res => res.json())
+            .then(data => setProducts(data));
+    }, [])
     return (
         <div>
-            <h2>This is shop</h2>
+            <div className="shop-container">
+                <div className="product-container">
+                    <h2>Products: {products.length}</h2>
+                    {
+                        products.map(product => <Product product={product}></Product>)
+                    }
+                </div>
+                <div className="cart-container">
+                    <h3>Order Summary</h3>
+                    <h3>Items Orders:</h3>
+                </div>
+            </div>
         </div>
     );
 };
